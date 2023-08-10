8207230b7e2fc63a713f0388c9066e4f00df8673
Danil Kostromin <limonikas@me.com>
2023-08-11 02:39:51 +0300

    feat: add tamagui

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,11 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
+import { Stack, Text } from "tamagui";
 
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
+    <Stack flex={1} alignItems="center" justifyContent="center">
+      <Text fontSize={20} fontWeight="bold" color="$red10">
+        Tab One
+      </Text>
+    </Stack>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,28 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
     plugins: [
       // Required for expo-router
-      'expo-router/babel',
+      "expo-router/babel",
+      // NOTE: this is required to pass the right environment
+      [
+        "transform-inline-environment-variables",
+        // NOTE: include is optional, you can leave this part out
+        {
+          include: ["TAMAGUI_TARGET", "EXPO_ROUTER_APP_ROOT"],
+        },
+      ],
+      [
+        "@tamagui/babel-plugin",
+        {
+          components: ["tamagui"],
+          config: "./tamagui.config.ts",
+          logTimings: true,
+        },
+      ],
+      // NOTE: this is only necessary if you are using reanimated for animations
+      "react-native-reanimated/plugin",
     ],
   };
 };
